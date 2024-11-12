5dc3ad5e87c5d21a503a3a1e135b033d761cdcc5
Slesarev Aleksei <lexsl84@gmail.com>
2024-11-12 20:22:35 +0100

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Алексей Слесарев on 12.11.24..
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
